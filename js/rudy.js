ca032280832f230311c15eb70cda2fa8e98df76f
rudymemo <rudydiff@126.com>
2020-09-08 17:50:56 +0800

    localFile

--- js/rudy.js
+++ js/rudy.js
@@ -10,14 +10,14 @@ const ap5 = new APlayer({
     audio: [{
         name: 'Passi',
         artist: 'Bruno Bavota',
-        url: 'https://s-bj-1828-rudy.oss.dogecdn.com/music%2FPassi%20-%20Bruno%20Bavota.mp3',
-        cover: 'https://s-bj-1828-rudy.oss.dogecdn.com/img/Passi.png',
-        lrc: 'https://s-bj-1828-rudy.oss.dogecdn.com/lrc/nolrc.lrc'
+        url: '../music/Passi - Bruno Bavota.mp3',
+        cover: '../img/Passi.png',
+        lrc: '../lrc/nolrc.lrc'
     }, {
         name: 'Together At Last',
         artist: 'Alexis Ffrench',
-        url: 'https://s-bj-1828-rudy.oss.dogecdn.com/music%2FTogether%20At%20Last%20-%20Alexis%20Ffrench.mp3',
-        cover: 'https://s-bj-1828-rudy.oss.dogecdn.com/img/TogetherAtLast.jpg',
-        lrc: 'https://s-bj-1828-rudy.oss.dogecdn.com/lrc/nolrc.lrc'
+        url: '../music/Together At Last - Alexis Ffrench.mp3',
+        cover: '../img/TogetherAtLast.jpg',
+        lrc: '../lrc/nolrc.lrc'
     }]
 });
