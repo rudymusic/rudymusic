d15576f3f63ba1cc981997e1479703f91807e66a
rudymemo <rudydiff@126.com>
2021-07-26 14:39:44 +0800

    add some music

--- js/rudy.js
+++ js/rudy.js
@@ -8,10 +8,82 @@ const ap5 = new APlayer({
     listFolded: false,
     listMaxHeight: 80,
     audio: [{
+        name: 'Denis Laurent - Shine',
+        artist: 'Denis Laurent',
+        url: '../music/Denis Laurent - Shine.mp3',
+        cover: '../img/Denis Laurent - Shine.jpg',
+        lrc: '../lrc/nolrc.lrc'
+    },{
+        name: 'Paul Cardall - Gracie`s Theme',
+        artist: 'Paul Cardall',
+        url: '../music/Paul Cardall - Gracie`s Theme.mp3',
+        cover: '../img/Paul Cardall - Gracie`s Theme.jpg',
+        lrc: '../lrc/nolrc.lrc'
+    },{
+        name: 'Paul Cardall - Letting Go',
+        artist: 'Paul Cardall',
+        url: '../music/Paul Cardall - Letting Go.mp3',
+        cover: '../img/Paul Cardall - Letting Go.jpg',
+        lrc: '../lrc/nolrc.lrc'
+    },{
+        name: 'Paul Cardall - New Life',
+        artist: 'Paul Cardall',
+        url: '../music/Paul Cardall - New Life.mp3',
+        cover: '../img/Paul Cardall - New Life.jpg',
+        lrc: '../lrc/nolrc.lrc'
+    },{
         name: 'Passi',
         artist: 'Bruno Bavota',
         url: '../music/Passi - Bruno Bavota.mp3',
         cover: '../img/Passi.png',
+        lrc: '../lrc/nolrc.lrc'
+    },{
+        name: 'Paul Cardall - Sign of Affection',
+        artist: 'Paul Cardall',
+        url: '../music/Paul Cardall - Sign of Affection.mp3',
+        cover: '../img/Paul Cardall - Sign of Affection.jpg',
+        lrc: '../lrc/nolrc.lrc'
+    },{
+        name: 'Postiljonen - On the Run',
+        artist: 'Postiljonen',
+        url: '../music/Postiljonen - On the Run.mp3',
+        cover: '../img/Postiljonen - On the Run.jpg',
+        lrc: '../lrc/nolrc.lrc'
+    },{
+        name: 'Sunny Choi - Happily Ever After',
+        artist: 'Sunny Choi',
+        url: '../music/Sunny Choi - Happily Ever After.mp3',
+        cover: '../img/Sunny Choi - Happily Ever After.jpg',
+        lrc: '../lrc/nolrc.lrc'
+    },{
+        name: 'The Midnight - Last Train',
+        artist: 'The Midnight',
+        url: '../music/The Midnight - Last Train.mp3',
+        cover: '../img/The Midnight - Last Train.jpg',
+        lrc: '../lrc/nolrc.lrc'
+    },{
+        name: 'The Midnight - Los Angeles',
+        artist: 'The Midnight',
+        url: '../music/The Midnight - Los Angeles.mp3',
+        cover: '../img/The Midnight - Los Angeles.jpg',
+        lrc: '../lrc/nolrc.lrc'
+    },{
+        name: 'Timecop1983 - Echoes',
+        artist: 'Timecop1983',
+        url: '../music/Timecop1983 - Echoes.mp3',
+        cover: '../img/Timecop1983 - Echoes',
+        lrc: '../lrc/nolrc.lrc'
+    },{
+        name: 'Timecop1983 - Fading Memories',
+        artist: 'Timecop1983',
+        url: '../music/Timecop1983 - Fading Memories.mp3',
+        cover: '../img/Timecop1983 - Fading Memories.jpg',
+        lrc: '../lrc/nolrc.lrc'
+    },{
+        name: 'Timecop1983 - Lonely Nights',
+        artist: 'Timecop1983',
+        url: '../music/Timecop1983 - Lonely Nights.mp3',
+        cover: '../img/Timecop1983 - Lonely Nights.jpg',
         lrc: '../lrc/nolrc.lrc'
     }, {
         name: 'Together At Last',
