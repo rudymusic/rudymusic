7e20c0b13a72e033bbd2f6e5d29bb729945a7c1d
rudymemo <rudydiff@126.com>
2021-07-28 14:44:49 +0800

    cover and mp3 from cdn

--- js/rudy.js
+++ js/rudy.js
@@ -7,95 +7,13 @@ const ap5 = new APlayer({
     theme: '#e9e9e9',
     listFolded: false,
     listMaxHeight: 80,
+    // 封面来自https://sm.ms网站图床
+    // url来自icloud共享
     audio: [{
-        name: 'Denis Laurent - Shine',
-        artist: 'Denis Laurent',
-        url: '../music/Denis Laurent - Shine.mp3',
-        cover: '../img/Denis Laurent - Shine.jpg',
-        lrc: '../lrc/nolrc.lrc'
-    },{
-        name: 'Paul Cardall - Gracie`s Theme',
-        artist: 'Paul Cardall',
-        url: '../music/Paul Cardall - Gracie`s Theme.mp3',
-        cover: '../img/Paul Cardall - Gracie`s Theme.jpg',
-        lrc: '../lrc/nolrc.lrc'
-    },{
-        name: 'Paul Cardall - Letting Go',
-        artist: 'Paul Cardall',
-        url: '../music/Paul Cardall - Letting Go.mp3',
-        cover: '../img/Paul Cardall - Letting Go.jpg',
-        lrc: '../lrc/nolrc.lrc'
-    },{
-        name: 'Paul Cardall - New Life',
-        artist: 'Paul Cardall',
-        url: '../music/Paul Cardall - New Life.mp3',
-        cover: '../img/Paul Cardall - New Life.jpg',
-        lrc: '../lrc/nolrc.lrc'
-    },{
-        name: 'Passi',
-        artist: 'Bruno Bavota',
-        url: '../music/Passi - Bruno Bavota.mp3',
-        cover: '../img/Passi.png',
-        lrc: '../lrc/nolrc.lrc'
-    },{
-        name: 'Paul Cardall - Sign of Affection',
-        artist: 'Paul Cardall',
-        url: '../music/Paul Cardall - Sign of Affection.mp3',
-        cover: '../img/Paul Cardall - Sign of Affection.jpg',
-        lrc: '../lrc/nolrc.lrc'
-    },{
-        name: 'Postiljonen - On the Run',
-        artist: 'Postiljonen',
-        url: '../music/Postiljonen - On the Run.mp3',
-        cover: '../img/Postiljonen - On the Run.jpg',
-        lrc: '../lrc/nolrc.lrc'
-    },{
-        name: 'Sunny Choi - Happily Ever After',
-        artist: 'Sunny Choi',
-        url: '../music/Sunny Choi - Happily Ever After.mp3',
-        cover: '../img/Sunny Choi - Happily Ever After.jpg',
-        lrc: '../lrc/nolrc.lrc'
-    },{
-        name: 'The Midnight - Last Train',
-        artist: 'The Midnight',
-        url: '../music/The Midnight - Last Train.mp3',
-        cover: '../img/The Midnight - Last Train.jpg',
-        lrc: '../lrc/nolrc.lrc'
-    },{
-        name: 'The Midnight - Los Angeles',
-        artist: 'The Midnight',
-        url: '../music/The Midnight - Los Angeles.mp3',
-        cover: '../img/The Midnight - Los Angeles.jpg',
-        lrc: '../lrc/nolrc.lrc'
-    },{
-        name: 'Timecop1983 - Echoes',
-        artist: 'Timecop1983',
-        url: '../music/Timecop1983 - Echoes.mp3',
-        cover: '../img/Timecop1983 - Echoes',
-        lrc: '../lrc/nolrc.lrc'
-    },{
-        name: 'Timecop1983 - Fading Memories',
-        artist: 'Timecop1983',
-        url: '../music/Timecop1983 - Fading Memories.mp3',
-        cover: '../img/Timecop1983 - Fading Memories.jpg',
-        lrc: '../lrc/nolrc.lrc'
-    },{
-        name: 'Timecop1983 - Lonely Nights',
-        artist: 'Timecop1983',
-        url: '../music/Timecop1983 - Lonely Nights.mp3',
-        cover: '../img/Timecop1983 - Lonely Nights.jpg',
-        lrc: '../lrc/nolrc.lrc'
-    }, {
-        name: 'Together At Last',
-        artist: 'Alexis Ffrench',
-        url: '../music/Together At Last - Alexis Ffrench.mp3',
-        cover: '../img/TogetherAtLast.jpg',
-        lrc: '../lrc/nolrc.lrc'
-    }, {
-        name: '本当の优しさとは…',
-        artist: '渡辺俊幸',
-        url: '../music/本当の优しさとは… - 渡辺俊幸.mp3',
-        cover: '../img/本当の优しさとは… - 渡辺俊幸.jpg',
+        name: 'Take the Time',
+        artist: 'Robin Spielberg',
+        url: 'https://cvws.icloud-content.com/B/AdZgPhl8IPMPwQ6--L6SnPLbklfFAccAYauUOv3pIMkR1I2gMGUENkUs/Robin%20Spielberg%20-%20Take%20the%20Time.mp3?o=AhWF1ivDVx41TAtP7bbiNxY7goJ1SI6yzahOj0PXw1V6&v=1&x=3&a=CAogu2JfzzoISjtAGZfByOMWxkjuOKLP3EbAEU179e1eYOwSbRC67trdri8Yusu2364vIgEAUgTbklfFWgQENkUsaib1vwTUR9zVT7LkDszuRRSVCFVnZcbdDgYdbsgLJLSOJMth5EAjEHImWaBUfTLhfS14ZmegxqVV7XEK76-7LOUd4ETKEtS2p8gxu5lPWTQ&e=1627455858&fl=&r=4db694c5-7ddb-4792-b278-f67cb8a574ba-1&k=LSpqb_1D1D2X0Ppg66cJfQ&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=34&s=XqW68xapK-syfFXTbkE73XdDPsw&%20=f22ae175-e728-4c29-8f3e-2b5db533a52b',
+        cover: 'https://i.loli.net/2021/07/28/NoQ192AMJSxPyz7.png',
         lrc: '../lrc/nolrc.lrc'
     }]
 });
