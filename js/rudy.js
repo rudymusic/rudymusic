1f68dec5a3d0761464e95c00aa5d2ba895b185ea
rudymemo <rudydiff@126.com>
2021-07-29 17:23:03 +0800

    change mp3 address

--- js/rudy.js
+++ js/rudy.js
@@ -12,7 +12,7 @@ const ap5 = new APlayer({
     audio: [{
         name: 'Take the Time',
         artist: 'Robin Spielberg',
-        url: 'https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBc0tMRTg5YmpGTnVtaUE2Vk9QRkplYUNmOWRfP2U9UTBiRmhO.mp3',
+        url: 'https://o67c9q.bn.files.1drv.com/y4m1LRZeZC66imfZmUExSMasp0A6ZgGiWYw4sNinKJ3-jYWAMGWiDtRz7Ayi41bTcVKBMBJ8P04TQwD44kBUvzbMdSzFwDiHO40r3zdlssRaIW8JRbAhBtTb3HA-2tosVTVVhgaTy9sKHBcxuAvE67lqKGPNcTOBADdrWktO2rqNDfg3WlDjLykFbSnP5R-nQytJaFssrCvOJQTTZP22CoIng',
         cover: 'https://i.loli.net/2021/07/28/NoQ192AMJSxPyz7.png',
         lrc: '../lrc/nolrc.lrc'
     }]
